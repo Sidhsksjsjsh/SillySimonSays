8ce744bfaa60b6a404c1c7cfefa9cae042e5716e
Ambatukam <117529823+Sidhsksjsjsh@users.noreply.github.com>
2023-06-27 07:43:14 +0900

    Update CheatingTool.lua

--- CheatingTool.lua
+++ CheatingTool.lua
@@ -11,6 +11,12 @@ next update:
 function tp_client(strip)
 game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(strip.Position)
 end
+local tiles = {}
+function rcreatetable(Table_string,localname)
+for _,v in pairs(Table_string:GetChildren()) do
+        table.insert(localname,v.Name)
+    end
+end
 
 Tab1:CreateButton("farm orb score", function()
 for i,v in pairs(game.Workspace:GetDescendants()) do
@@ -41,10 +47,17 @@ for i,v in pairs(game.Workspace:GetDescendants()) do
         end
     end
 end
-if game:GetService("Workspace").Arena:FindFirstChild("Head"):FindFirstChild("Head") then
+--[[if game:GetService("Workspace").Arena:FindFirstChild("Head"):FindFirstChild("Head") then
 tp_client(game:GetService("Workspace").Arena.Head.Head)
 end
 if game:GetService("Workspace").MiniGameObjects:FindFirstChild("Head"):FindFirstChild("Head") then
 tp_client(game:GetService("Workspace").MiniGameObjects.Head.Head)
+end--]]
+if game.Workspace.MiniGameObjects:FindFirstChild("Dummy"):FindFirstChild("CollisionBox") then
+game:GetService("ReplicatedStorage").Events.NotifyServerStompedObject:FireServer(game.Workspace.MiniGameObjects.Dummy.CollisionBox)
+end
+if game.Workspace:FindFirstChild("Scene") then
+rcreatetable(game.Workspace.Scene,tiles)
+game:GetService("ReplicatedStorage").Events.SubmitToServerMiniGameAnswer:FireServer(game.Workspace.Scene[tiles[math.random(1,#tiles)]],game.Workspace.Scene[tiles[math.random(1,#tiles)]])
 end
 end)
